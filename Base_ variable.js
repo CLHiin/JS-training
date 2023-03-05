7bcdad1d8e711e73add995a53899b8f07a3fd0c2
CLHiin <iin756723cs@gmail.com>
2023-03-05 14:22:33 +0800

    Summayr

---
+++ Base_ variable.js
@@ -0,0 +1,17 @@
+//字串
+"用雙引號包住"
+    
+//數字
+8787 & 987.654 & 0.123
+
+//布林值
+true & false
+
+//變數 -只能是 [英文] or [數字] or [ $ ] or [ _ ] 的組合,開頭不能是數字
+var dog ="布魯托" , age = 15 , rule = true;
+
+document.write(dog);
+document.write("<br/>");//可以用 " html標籤 " 來使用html的標籤效果
+document.write("有條狗叫做"+dog);//可以用+來讓字串弄在一起
+document.write("<br/>"+ age );
+document.write("<br/>"+ rule);
